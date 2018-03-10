b8ee44af4a15ac84084ecd923fe3f8a5f0b35d44
aakashvarma18 <aakashvarma18@gmail.com>
2018-03-11 02:12:17 +0530

    word_tokenization added

--- text.py
+++ text.py
@@ -6,7 +6,7 @@ from nltk.stem import PorterStemmer
 from nltk.tokenize import RegexpTokenizer
 from nltk.tokenize import PunktSentenceTokenizer
 
-data = "Potholes have been causing a lot of inconvenience in Bangalore, there has already been a lot of accidents in the place bacause of these potholes. Please look into this matter."
+data = "Potholes have been causing a lot of inconvenience in Bangalore, there has already been a lot of accidents in the place bacause of these potholes, please look into this matter."
 # print(word_tokenize(data))
 
 tokenizer = RegexpTokenizer(r'\w+')
@@ -19,19 +19,27 @@ wordsFiltered = []
 for w in words:
     if w not in stopWords:
         wordsFiltered.append(w)
-
 print (wordsFiltered)
 
-stemWords = []
-ps = PorterStemmer()
- 
-for word in wordsFiltered:
-    stemWords.append(ps.stem(word))
-
-# print (stemWords)
 
 tagged = nltk.pos_tag(wordsFiltered)
 print (tagged)
+
+#############################################################################
+
+sentences = nltk.sent_tokenize(data)   
+ 
+d = []
+for sent in sentences:
+    d = d + nltk.pos_tag(nltk.word_tokenize(sent))
+ 
+for word in d: 
+    if 'NNP' in word[1]: 
+        print(word)
+
+#############################################################################
+
+
 
 
 
