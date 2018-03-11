fb139ea3b33a79380616ffa1163959db992cf034
aakashvarma18 <aakashvarma18@gmail.com>
2018-03-11 14:13:58 +0530

    Api looping done

--- apidump.py
+++ apidump.py
@@ -7,4 +7,7 @@ url = "http://letsrant.azurewebsites.net/api/values"
 reader = codecs.getreader("utf-8")
 obj = json.load(reader(urlopen(url)))
 
-print (obj)
+for i in range(0,len(obj)):
+    print (obj[i])
+
+# print (obj[1])
